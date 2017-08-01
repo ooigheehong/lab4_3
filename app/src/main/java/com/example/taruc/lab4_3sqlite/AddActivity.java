92dadf154ec936adac138f007d34c3131a015f50
taruc <ooigheehong@hotmail.com>
2017-08-01 09:56:27 +0800

    done

---
+++ app/src/main/java/com/example/taruc/lab4_3sqlite/AddActivity.java
@@ -0,0 +1,35 @@
+package com.example.taruc.lab4_3sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText editTextName, editTextPhone, editTextEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        editTextName = (EditText)findViewById(R.id.editTextName);
+        editTextPhone = (EditText)findViewById(R.id.editTextPhone);
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+
+    }
+
+    public void saveRecord(View v){
+
+        UserRecord userRecord = new UserRecord();
+        userRecord.setPhone(editTextPhone.getText().toString());
+        userRecord.setName(editTextName.getText().toString());
+        userRecord.setEmail(editTextEmail.getText().toString());
+
+        UserSQLHelper userDataSource = new UserSQLHelper(this);
+        userDataSource.insertUser(userRecord);
+
+        this.finish(); //Terminate this Activity
+    }
+}
